731dbfd696a7ae33e5a2f8177b6e4bc05ac45102
LucienPrabhat <ww28xx@gmail.com>
2018-09-03 14:53:31 +0800

    modify the duplicated 'module.exports'

--- controllers/Default.js
+++ controllers/Default.js
@@ -13,25 +13,25 @@ function _errHandler(response){
 
 //MUTEX
 
-module.exports.lockMutex = function lockMutex (req, res, next) {
+function lockMutex (req, res, next) {
   let mutexKey = req.swagger.params['mutexKey'].value;
   let mutexttl = req.swagger.params['mutexttl'].value;
   Default.lockMutex(mutexKey,mutexttl).then(_response).catch(_errHandler);
 };
 
-module.exports.unlockMutex = function unlockMutex (req, res, next) {
+function unlockMutex (req, res, next) {
   let mutexKey = req.swagger.params['mutexKey'].value;
   let mutexHandle = req.swagger.params['mutexHandle'].value;
   Default.unlockMutex(mutexKey,mutexHandle).then(_response).catch(_errHandler);
 };
 
-module.exports.extendMutex = function extendMutex (req, res, next) {
+function extendMutex (req, res, next) {
   let mutexKey = req.swagger.params['mutexKey'].value;
   let mutexArg = req.swagger.params['mutexArg'].value;
   Default.extendMutex(mutexKey,mutexArg).then(_response).catch(_errHandler);
 };
 
-module.exports.queryMutex = function queryMutex (req, res, next) {
+function queryMutex (req, res, next) {
   let mutexKey = req.swagger.params['mutexKey'].value;
   Default.queryMutex(mutexKey).then(_response).catch(_errHandler);
 };
@@ -39,37 +39,50 @@ module.exports.queryMutex = function queryMutex (req, res, next) {
 
 //SEMAPHORE
 
-module.exports.createSema = function createSema (req, res, next) {
+function createSema (req, res, next) {
   let semaKey = req.swagger.params['semaKey'].value;
   let semaLockargs = req.swagger.params['semaLockargs'].value;
   Default.createSema(semaKey,semaLockargs).then(_response).catch(_errHandler);
 };
 
-module.exports.deleteSema = function deleteSema (req, res, next) {
+function deleteSema (req, res, next) {
   let semaKey = req.swagger.params['semaKey'].value;
   Default.deleteSema(semaKey).then(_response).catch(_errHandler);
 };
 
-module.exports.aquireSeat = function aquireSeat (req, res, next) {
+function aquireSeat (req, res, next) {
   let semaKey = req.swagger.params['semaKey'].value;
   let semaSeatttl = req.swagger.params['semaSeatttl'].value;
   Default.aquireSeat(semaKey,semaSeatttl).then(_response).catch(_errHandler);
 };
 
-module.exports.releaseSeat = function releaseSeat (req, res, next) {
+function releaseSeat (req, res, next) {
   let semaKey = req.swagger.params['semaKey'].value;
   let SemaHandle = req.swagger.params['SemaHandle'].value;
   Default.releaseSeat(semaKey,SemaHandle).then(_response).catch(_errHandler);
 };
 
-module.exports.postponeSeat = function postponeSeat (req, res, next) {
+function postponeSeat (req, res, next) {
   let semaKey = req.swagger.params['semaKey'].value;
   let semaSeatArgs = req.swagger.params['semaSeatArgs'].value;
   Default.postponeSeat(semaKey,semaSeatArgs).then(_response).catch(_errHandler);
 };
 
-module.exports.querySema = function querySema (req, res, next) {
+function querySema (req, res, next) {
   let semaKey = req.swagger.params['semaKey'].value;
   Default.querySema(semaKey).then(_response).catch(_errHandler);
 };
+
+module.exports={
+  lockMutex,
+  unlockMutex,
+  extendMutex,
+  queryMutex,
+  createSema,
+  deleteSema,
+  aquireSeat,
+  releaseSeat,
+  postponeSeat,
+  querySema,
+}
 
