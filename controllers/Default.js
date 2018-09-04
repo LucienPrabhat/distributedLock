874a323edd1513b055a1feecd1055df42cc67678
LucienPrabhat <ww28xx@gmail.com>
2018-09-04 18:45:07 +0800

    handling error and wrapping the duplicate function

--- controllers/Default.js
+++ controllers/Default.js
@@ -1,76 +1,65 @@
 let utils = require('../utils/writer.js');
 let Default = require('../service/DefaultService');
+let _res;
 
-//functions
-function _response(response){
-  utils.writeJson(res, {"message":response['msg']}, response['statusCode']);
+//param wrapper
+function _param(req,res,param1,param2){
+  _res=res;
+  if(param2===undefined) return [req.swagger.params[param1].value];
+  return [req.swagger.params[param1].value,req.swagger.params[param2].value];
 }
-
+//reaponse wrapper
+function _response(response){
+  utils.writeJson(_res, {"message":response['msg']}, response['statusCode']);
+}
 function _errHandler(response){
-  utils.writeJson(res, response);
+  utils.writeJson(_res, response);
 }
 
 
 //MUTEX
-
 function lockMutex (req, res, next) {
-  let mutexKey = req.swagger.params['mutexKey'].value;
-  let mutexttl = req.swagger.params['mutexttl'].value;
-  Default.lockMutex(mutexKey,mutexttl).then(_response).catch(_errHandler);
+  let param=_param(req,res,'mutexKey','mutexttl')
+  Default.lockMutex(param[0],param[1]).then(_response).catch(_errHandler);
 };
-
 function unlockMutex (req, res, next) {
-  let mutexKey = req.swagger.params['mutexKey'].value;
-  let mutexHandle = req.swagger.params['mutexHandle'].value;
-  Default.unlockMutex(mutexKey,mutexHandle).then(_response).catch(_errHandler);
+  let param=_param(req,res,'mutexKey','mutexHandle')
+  Default.unlockMutex(param[0],param[1]).then(_response).catch(_errHandler);
 };
-
 function extendMutex (req, res, next) {
-  let mutexKey = req.swagger.params['mutexKey'].value;
-  let mutexArg = req.swagger.params['mutexArg'].value;
-  Default.extendMutex(mutexKey,mutexArg).then(_response).catch(_errHandler);
+  let param=_param(req,res,'mutexKey','mutexArg')
+  Default.extendMutex(param[0],param[1]).then(_response).catch(_errHandler);
 };
-
 function queryMutex (req, res, next) {
-  let mutexKey = req.swagger.params['mutexKey'].value;
-  Default.queryMutex(mutexKey).then(_response).catch(_errHandler);
+  let param=_param(req,res,'mutexKey')
+  Default.queryMutex(param[0]).then(_response).catch(_errHandler);
 };
 
 
 //SEMAPHORE
-
 function createSema (req, res, next) {
-  let semaKey = req.swagger.params['semaKey'].value;
-  let semaLockargs = req.swagger.params['semaLockargs'].value;
-  Default.createSema(semaKey,semaLockargs).then(_response).catch(_errHandler);
+  let param=_param(req,res,'semaKey','semaLockargs')
+  Default.createSema(param[0],param[1]).then(_response).catch(_errHandler);
 };
-
 function deleteSema (req, res, next) {
-  let semaKey = req.swagger.params['semaKey'].value;
-  Default.deleteSema(semaKey).then(_response).catch(_errHandler);
+  let param=_param(req,res,'semaKey')
+  Default.deleteSema(param[0]).then(_response).catch(_errHandler);
 };
-
 function aquireSeat (req, res, next) {
-  let semaKey = req.swagger.params['semaKey'].value;
-  let semaSeatttl = req.swagger.params['semaSeatttl'].value;
-  Default.aquireSeat(semaKey,semaSeatttl).then(_response).catch(_errHandler);
+  let param=_param(req,res,'semaKey','semaSeatttl')
+  Default.aquireSeat(param[0],param[1]).then(_response).catch(_errHandler);
 };
-
 function releaseSeat (req, res, next) {
-  let semaKey = req.swagger.params['semaKey'].value;
-  let SemaHandle = req.swagger.params['SemaHandle'].value;
-  Default.releaseSeat(semaKey,SemaHandle).then(_response).catch(_errHandler);
+  let param=_param(req,res,'semaKey','SemaHandle')
+  Default.releaseSeat(param[0],param[1]).then(_response).catch(_errHandler);
 };
-
 function postponeSeat (req, res, next) {
-  let semaKey = req.swagger.params['semaKey'].value;
-  let semaSeatArgs = req.swagger.params['semaSeatArgs'].value;
-  Default.postponeSeat(semaKey,semaSeatArgs).then(_response).catch(_errHandler);
+  let param=_param(req,res,'semaKey','semaSeatArgs')
+  Default.postponeSeat(param[0],param[1]).then(_response).catch(_errHandler);
 };
-
 function querySema (req, res, next) {
-  let semaKey = req.swagger.params['semaKey'].value;
-  Default.querySema(semaKey).then(_response).catch(_errHandler);
+  let param=_param(req,res,'semaKey')
+  Default.querySema(param[0]).then(_response).catch(_errHandler);
 };
 
 module.exports={
